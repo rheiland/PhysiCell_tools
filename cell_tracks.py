744efaab1ad32562afa0f29be0b677da84affb14
Randy Heiland <heiland@indiana.edu>
2017-11-26 17:27:00 -0500

    initial commit

---
+++ cell_tracks.py
@@ -0,0 +1,91 @@
+#
+# cell_tracks.py - plot 2-D cell tracks associated with PhysiCell .svg files
+#
+import sys
+import xml.etree.ElementTree as ET
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+
+d={}   # dictionary to hold all (x,y) positions of cells
+
+""" 
+--- for example ---
+In [141]: d['cell1599'][0:3]
+Out[141]: 
+array([[ 4900.  ,  4900.  ],
+       [ 4934.17,  4487.91],
+       [ 4960.75,  4148.02]])
+"""
+
+count = 0
+for fname in glob.glob('snapshot*.svg'):
+#for fname in['snapshot00000000.svg', 'snapshot00000001.svg']:
+#for fname in['snapshot00000000.svg']:
+  print(fname)
+  count += 1
+  if count > 99999:
+    break
+
+  print('\n---- ' + fname + ':')
+  tree=ET.parse(fname)
+
+#  print('--- root.tag, root.attrib ---')
+  root=tree.getroot()
+#  print('--- root.tag ---')
+#  print(root.tag)
+#  print('--- root.attrib ---')
+#  print(root.attrib)
+
+
+#  print('--- child.tag, child.attrib ---')
+  numChildren = 0
+  for child in root:
+#  print(child.tag, child.attrib)
+#    print('attrib=',child.attrib)
+#  if (child.attrib['id'] == 'tissue'):
+    if ('id' in child.attrib.keys()):
+      print('-------- found tissue!!')
+      tissue_parent = child
+      break
+
+#  print('------ search tissue')
+  for child in tissue_parent:
+#    print('attrib=',child.attrib)
+    if (child.attrib['id'] == 'cells'):
+      print('-------- found cells!!')
+      cells_parent = child
+      break
+    numChildren += 1
+
+
+  num_cells = 0
+#  print('------ search cells')
+  for child in cells_parent:
+#    print(child.tag, child.attrib)
+#    print('attrib=',child.attrib)
+    for circle in child:
+#      print('  --- cx,cy=',circle.attrib['cx'],circle.attrib['cy'])
+      if (child.attrib['id'] in d.keys()):
+        d[child.attrib['id']] = np.vstack((d[child.attrib['id']], [ float(circle.attrib['cx']), float(circle.attrib['cy']) ]))
+      else:
+        d[child.attrib['id']] = np.array( [ float(circle.attrib['cx']), float(circle.attrib['cy']) ])
+      break
+#    if (child.attrib['id'] == 'cells'):
+#      print('-------- found cells!!')
+#      tissue_child = child
+    num_cells += 1
+  print('num_cells= ',num_cells)
+
+
+fig = plt.figure(figsize=(8,8))
+#print 'dir(fig)=',dir(fig)
+#fig.set_figwidth(8)
+#fig.set_figheight(8)
+
+for key in d.keys():
+  x = d[key][:,0]
+  y = d[key][:,1]
+  plt.plot(x,y)
+
+plt.show()
