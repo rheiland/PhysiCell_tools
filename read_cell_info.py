a4e6d781d33992cd1d85c838766b915ff9d0e5e0
Randy Heiland <heiland@indiana.edu>
2019-05-29 12:11:39 -0400

    improve

--- read_cell_info.py
+++ read_cell_info.py
@@ -20,13 +20,27 @@ print("num_cells =",num_cells)
 max_cells = num_cells 
 max_cells = 40 
 
+show_nucleus = False  # should only need if doing "cut" slices through the cells
+
 for idx in range(0, num_cells):
   # rf. PhysiCell User Guide for these array indices to a cell's position.
   x = val[1,idx]
   y = -val[2,idx]  # invert Y (points down)
   z = val[3,idx]
 
-  sval = 0   # immune cells are black??
+  """
+  - from the User_Guide.pdf:
+   false cell coloring Ki67 is recommended for the Ki67 Basic and Ki67 Advanced cycle models.
+   (See Section 11.1 and Section 17.1.) Pre-mitotic Ki67+ cells are colored green (0,255,0), with a
+   darker green nucleus (0,125,0). Post-mitotic Ki67+ cells are colored magenta (255,0,255), with a
+   darker magenta nucleus (125,0,125). (In the Ki67 Basic model, all Ki67+ cells are green.) Ki67- cells
+   are colored blue (40,200,255) with a darker blue nucleus (20,100,255).
+   Apoptotic cells are colored red (255,0,0) with a darker red nucleus (125,0,0). Necrotic cells are colored
+   brown (250,138,38) with a darker brown nucleus (139,69,19). All outlines are black.
+  """
+
+  # for now, rather than provide RGB colors, just provide an integer value that can be mapped to a color
+  sval = 0   # default color
   if val[5,idx] == 1:  # immune cell type
     sval = 1   # lime green 
   if val[7,idx] > 100 and val[7,idx] < 104:
@@ -34,8 +48,15 @@ for idx in range(0, num_cells):
   elif val[7,idx] == 100:
     sval = 3   # apoptotic: red
 
-  radius = (val[4,idx]*0.2387)**0.333 
+  total_volume = val[4,idx]
+  cell_radius = (total_volume*0.2387)**0.333   # Volume = (4/3)*pi*r^3
 
   if (idx < max_cells):
-    print(x,y,z,radius)
+    print(x,',',y,',',z,',',cell_radius, ',',sval)
+
+  # fix up if ever used
+  if (show_nucleus):
+    nuclear_volume = val[9,idx]
+    nuclear_radius = (nuclear_volume*0.2387)**0.333 
+    print(x,y,z,nuclear_radius)
 
