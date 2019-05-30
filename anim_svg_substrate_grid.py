aad72bb4b2296abd5cec664ea4638ae17087262d
Randy Heiland <heiland@indiana.edu>
2019-05-30 16:55:51 -0400

    use circle patches, not scatter

--- anim_svg_substrate_grid.py
+++ anim_svg_substrate_grid.py
@@ -16,6 +16,8 @@ import matplotlib
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
 from matplotlib.collections import LineCollection
+from matplotlib.patches import Circle, Ellipse, Rectangle
+from matplotlib.collections import PatchCollection
 import numpy as np
 from numpy.random import randn
 
@@ -128,6 +130,81 @@ count = -1
 
 cbar = None
 
+#-----------------------------------------------------
+def circles(x, y, s, c='b', vmin=None, vmax=None, **kwargs):
+    """
+    See https://gist.github.com/syrte/592a062c562cd2a98a83 
+
+    Make a scatter plot of circles. 
+    Similar to plt.scatter, but the size of circles are in data scale.
+    Parameters
+    ----------
+    x, y : scalar or array_like, shape (n, )
+        Input data
+    s : scalar or array_like, shape (n, ) 
+        Radius of circles.
+    c : color or sequence of color, optional, default : 'b'
+        `c` can be a single color format string, or a sequence of color
+        specifications of length `N`, or a sequence of `N` numbers to be
+        mapped to colors using the `cmap` and `norm` specified via kwargs.
+        Note that `c` should not be a single numeric RGB or RGBA sequence 
+        because that is indistinguishable from an array of values
+        to be colormapped. (If you insist, use `color` instead.)  
+        `c` can be a 2-D array in which the rows are RGB or RGBA, however. 
+    vmin, vmax : scalar, optional, default: None
+        `vmin` and `vmax` are used in conjunction with `norm` to normalize
+        luminance data.  If either are `None`, the min and max of the
+        color array is used.
+    kwargs : `~matplotlib.collections.Collection` properties
+        Eg. alpha, edgecolor(ec), facecolor(fc), linewidth(lw), linestyle(ls), 
+        norm, cmap, transform, etc.
+    Returns
+    -------
+    paths : `~matplotlib.collections.PathCollection`
+    Examples
+    --------
+    a = np.arange(11)
+    circles(a, a, s=a*0.2, c=a, alpha=0.5, ec='none')
+    plt.colorbar()
+    License
+    --------
+    This code is under [The BSD 3-Clause License]
+    (http://opensource.org/licenses/BSD-3-Clause)
+    """
+
+    if np.isscalar(c):
+        kwargs.setdefault('color', c)
+        c = None
+
+    if 'fc' in kwargs:
+        kwargs.setdefault('facecolor', kwargs.pop('fc'))
+    if 'ec' in kwargs:
+        kwargs.setdefault('edgecolor', kwargs.pop('ec'))
+    if 'ls' in kwargs:
+        kwargs.setdefault('linestyle', kwargs.pop('ls'))
+    if 'lw' in kwargs:
+        kwargs.setdefault('linewidth', kwargs.pop('lw'))
+    # You can set `facecolor` with an array for each patch,
+    # while you can only set `facecolors` with a value for all.
+
+    zipped = np.broadcast(x, y, s)
+    patches = [Circle((x_, y_), s_)
+               for x_, y_, s_ in zipped]
+    collection = PatchCollection(patches, **kwargs)
+    if c is not None:
+        c = np.broadcast_to(c, zipped.shape).ravel()
+        collection.set_array(c)
+        collection.set_clim(vmin, vmax)
+
+    ax = plt.gca()
+    ax.add_collection(collection)
+    ax.autoscale_view()
+    plt.draw_if_interactive()
+    if c is not None:
+        plt.sci(collection)
+    return collection
+
+#-----------------------------------------------------
 def plot_substrate():
     global current_idx, axes_max, cbar
 
@@ -325,9 +402,12 @@ def plot_svg():
   # axes range labels
   plt.xlim(xmin,xmax)
   plt.ylim(ymin,ymax)
-  plt.scatter(xvals,yvals, s=rvals*scale_radius, c=rgbs)
+  # plt.scatter(xvals,yvals, s=rvals*scale_radius, c=rgbs)
+  circles(xvals,yvals, s=rvals, color=rgbs, alpha=1.0, edgecolor='black')
+#  circles(xvals,yvals, s=rvals, color=rgbs)
 
 
+  # plot domain grid 
   xs = np.linspace(xmin,xmax,numx)
   ys = np.linspace(ymin,ymax,numy)
   hlines = np.column_stack(np.broadcast_arrays(xs[0], ys, xs[-1], ys))
