c7f0a7997e04a4ea6a56fe5a181681817e9f670b
Randy Heiland <heiland@indiana.edu>
2019-10-10 10:42:11 -0400

    multi files

--- cells_dist2D_metric.py
+++ cells_dist2D_metric.py
@@ -1,22 +1,26 @@
-# from pyMCDS import pyMCDS
+
 import numpy as np
 import scipy.io
 
-cells_dict = {}
-iframe = 0
-fname = "output%08d_cells_physicell" % iframe
-print("fname = ",fname)
-scipy.io.loadmat(fname, cells_dict)
-val = cells_dict['cells']
+def metric(idx):
+    global current_idx
 
-num_cells = val.shape[1]
-print("num_cells =",num_cells)
+    cells_dict = {}
+    iframe = 0
+    fname = "output%08d_cells_physicell" % idx
+    print("fname = ",fname)
+    scipy.io.loadmat(fname, cells_dict)
+    val = cells_dict['cells']
 
-xvals = val[1,:]
-yvals = val[2,:]
+    num_cells = val.shape[1]
+    print("num_cells =",num_cells)
 
-dist2 = xvals**2 + yvals**2
-dist = dist2**0.5
-print("avg dist of all cells = ",dist.sum()/len(dist))
+    xvals = val[1,:]
+    yvals = val[2,:]
 
-#print(mcds1.get_cell_variables())
+    dist2 = xvals**2 + yvals**2
+    dist = dist2**0.5
+    print("avg dist of all cells = ",dist.sum()/len(dist))
+
+for idx in range(0,18):
+    metric(idx)
