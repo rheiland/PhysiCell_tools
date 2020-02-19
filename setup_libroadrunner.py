525e7b8fb0a1298dd32938a16a69c7e52da48d2d
Randy Heiland <heiland@indiana.edu>
2020-02-18 19:56:55 -0500

    updates

--- setup_libroadrunner.py
+++ setup_libroadrunner.py
@@ -1,26 +1,71 @@
+# This script attempts to download the libRoadrunner (binary) libraries and
+# headers for your particular operating system. It puts them in a directory
+# of your choosing (default = your home dir) and sets an environment variable
+# to that location which can then be used by a PhysiCell Makefile.
+
 import platform
 import urllib.request
 import os
 import sys
+import tarfile
+import zipfile
 
 os_type = platform.system()
-print(os_type)
+print('os_type = ',os_type)
 
 # Assume Windows
-url = 'https://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.4.18/roadrunner-win64-vs14-cp35m.zip/download'
+rr_file = "roadrunner-win64-vs14-cp35m.zip"
+url = "https://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.4.18/" + rr_file + "/download"
 
 if os_type == 'Darwin':
-  url = 'https://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.4.18/roadrunner-osx-10.9-cp36m.tar.gz/download'
+    rr_file = "roadrunner-osx-10.9-cp36m.tar.gz"
+    url = "https://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.4.18/" + rr_file + "/download"
+#elif os_type == 'Windows':
 
 fname = url.split('/')[-2]
 
-print('Beginning download of libroadrunner into your home directory...')
+home = os.path.expanduser("~")
+print('libRoadRunner will now be installed into this location:')
+dir_name = os.path.join(home, 'libroadrunner')
+print(dir_name + '\n')
+print('   - Press ENTER to confirm the location')
+print('   - Press CTL-C to abort the installation')
+print('   - Or specify a different location below\n')
+prompt_str = '[' + dir_name + '] >>> '
+try:
+    response = input(prompt_str)
+    if (response == ""):
+        print('got Enter')
+        if not os.path.exists(dir_name):
+            try:
+                os.makedirs(dir_name)
+            except:
+                print('Error trying to create directory: ',dir_name)
+                exit(1)
+    else:
+        print(type(response))
+        dir_name = os.path.expanduser(response)
+        if not os.path.exists(dir_name):
+            try:
+                os.makedirs(dir_name)
+            except:
+                print('Error trying to create directory: ',dir_name)
+                exit(1)
+except:
+    print('  installation canceled\n')
+    exit(1)
+
+print('Beginning download of libroadrunner into ' + dir_name + ' ...')
 print(url)
 
-home = os.path.expanduser("~")
-my_file = os.path.join(home, fname)
+my_file = os.path.join(dir_name, fname)
+print('my_file = ',my_file)
 
-print('home = ',home)
+if os_type == 'Darwin':
+    rrlib_dir = my_file[:-7]
+elif os_type == 'Windows':
+    rrlib_dir = my_file[:-4]
+print('rrlib_dir = ',rrlib_dir)
 
 def download_cb(blocknum, blocksize, totalsize):
     readsofar = blocknum * blocksize
@@ -36,4 +81,28 @@ def download_cb(blocknum, blocksize, totalsize):
 
 urllib.request.urlretrieve(url, my_file, download_cb)
 
+os.chdir(dir_name)
+print('installing (uncompressing) the file...')
+if os_type == 'Darwin':
+    try:
+        tar = tarfile.open(rr_file)
+        tar.extractall()
+        tar.close()
+    except:
+        print('error untarring the file')
+        exit(1)
+#elif os_type == 'Darwin':
+elif os_type == 'Windows':
+    try:
+        with zipfile.ZipFile(rr_file) as zf:
+            zf.extractall('.')
+    except:
+        print('error unzipping the file')
+        exit(1)
 
+print('Done.\n')
+
+# LIBRR_DIR := /Users/heiland/libroadrunner/roadrunner-osx-10.9-cp36m
+print("Replace the 'LIBRR_DIR' variable in your PhysiCell Makefile with this:\n")
+#print("LIBRR_DIR := /Users/heiland/libroadrunner/roadrunner-osx-10.9-cp36m")
+print("LIBRR_DIR := " + rrlib_dir + "\n")
