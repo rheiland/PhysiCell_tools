cfeb951b9d4c7d1f96a4272e462f121cc5c857e4
Randy Heiland <heiland@indiana.edu>
2020-09-28 09:39:33 -0400

    update

--- params_run.py
+++ params_run.py
@@ -7,11 +7,10 @@
 
 import xml.etree.ElementTree as ET
 from shutil import copyfile
-#from subprocess import call
-import subprocess 
 import os
 import sys
 
+current_dir = 'foo'
 print(len(sys.argv))
 if (len(sys.argv) < 3):
   usage_str = "Usage: %s <pgm> <params.txt>" % (sys.argv[0])
@@ -28,10 +27,8 @@ xml_file_out = 'config/tmp.xml'
 copyfile(xml_file_in,xml_file_out)
 tree = ET.parse(xml_file_out)
 xml_root = tree.getroot()
-#d = {}
 first_time = True
 output_dirs = []
-#with open("param_runs.txt") as f:
 with open(params_file) as f:
     for line in f:
         print(len(line),line)
@@ -40,13 +37,16 @@ with open(params_file) as f:
         (key, val) = line.split()
         if (key == 'folder'):
             if first_time:  # we've read the 1st 'folder'
+                current_dir = val
                 first_time = False
             else:  # we've read  additional 'folder's
-#                first_time = True
                 # write the config file to the previous folder (output) dir and start a simulation
                 print('---write (previous) config file and start its sim')
                 tree.write(xml_file_out)
-                cmd =  pgm + " " + xml_file_out + " &"
+                # cmd =  pgm + " " + xml_file_out + " &"
+                cmd =  pgm + " " + xml_file_out + " > " + current_dir + ".log"
+                print(cmd)
+                current_dir = val
                 os.system(cmd)
 
             xml_file_out = val + '/config.xml'  # copy config file into the output dir
@@ -63,17 +63,16 @@ with open(params_file) as f:
             if (key == 'folder' and not os.path.exists(val)):
                 print('creating ' + val)
                 os.makedirs(val)
+                # current_dir = val
 
             xml_root.find('.//' + key).text = val
 
 tree.write(xml_file_out)
 
 #cwd = os.getcwd()
-cmd =  pgm + " " +  xml_file_out + " &"
+#cmd =  pgm + " " +  xml_file_out + " &"
+cmd =  pgm + " " + xml_file_out + " > " + current_dir + ".log"
+print(cmd)
 os.system(cmd)
 
 print(output_dirs)
-
-# python svg2other.py 0 120 1 25 jpg
-# mencoder "mf://snapshot*.jpg" -ovc lavc -lavcopts vcodec=mpeg4:vbitrate=10000:mbd=2:trell -mf fps=10:type=jpg -nosound -o foo.avi
-# convert foo.avi tiny_tumor_defaults.mp4
